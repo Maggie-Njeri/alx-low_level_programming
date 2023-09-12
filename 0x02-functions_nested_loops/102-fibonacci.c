3a7f1da884dcf005c2b64fa12312af04a8f00bad
Maggie-Njeri <mkiri00@gmail.com>
2023-09-12 15:45:11 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,21 +8,20 @@
 
 int main(void)
 {
-int fib1 = 0;
-int fib2 = 1;
-int next;
+unsigned long int fib1 = 0;
+unsigned long int fib2 = 1;
+unsigned long int next;
 int count;
 for (count = 0; count < 50; count++)
 {
-sum = fib1 + fib2;
-printf("%lu", sum);
+next = fib1 + fib2;
+printf("%lu", next);
 fib1 = fib2;
-fib2 = sum;
+fib2 = next;
 if (count == 49)
 printf("\n");
 else
 printf(", ");
 }
 return (0);
-}
 }
